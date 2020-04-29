0d17760870946fb356f16f028eb588b1a0dc532b
Taki Uddin <takiuddin93@gmail.com>
2020-04-29 22:28:13 +0600

    string added for groups

--- lib/constants/strings.dart
+++ lib/constants/strings.dart
@@ -1,7 +1,8 @@
-const String MESSAGES_COLLECTION = "messages";
 const String USERS_COLLECTION = "users";
-const String CALL_COLLECTION = "call";
 const String CONTACTS_COLLECTION = "contacts";
+const String MESSAGES_COLLECTION = "messages";
+const String GROUPS_COLLECTION = "groups";
+const String CALL_COLLECTION = "call";
 
 const String TIMESTAMP_FIELD = "timestamp";
 const String EMAIL_FIELD = "email";
