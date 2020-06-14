0f1dd6a66eba0eb5a43f21a736e5cf48fcd08960
Taki Uddin <takiuddin93@gmail.com>
2020-06-14 19:57:58 +0600

    color modifications

--- lib/utils/universal_variables.dart
+++ lib/utils/universal_variables.dart
@@ -1,19 +1,33 @@
 import 'package:flutter/material.dart';
 
 class UniversalVariables {
-  static final Color appBar = Color(0xFF1990E4);
-  static final Color appBarUserIcon = Color(0xFFCAEDFD);
+  static final Color primary = Color(0xFF5CC3FF);
+
+  static final Color appBar = Color(0xFFEBF8FF);
+  static final Color appBarUserIcon = Color(0xFF5CC3FF);
+
+  static final Color appBarBackIcon = Color(0xFF5CC3FF);
+  static final Color appBarNotificationsIcon = Color(0xFF5CC3FF);
+
+  static final Color bottomBarNavigation = Color(0xFF5CC3FF);
+
   static final Color whiteColor = Color(0xffffffff);
   static final Color transparentColor = Color(0x00FFFFFF);
-  static final Color blueColor = Color(0xff2b9ed4);
-  static final Color textColor = Color(0xFF007AE1);
+
+  static final Color blueColor = Color(0xFFADE1FF);
+  static final Color darkblueColor = Color(0xFF5CC3FF);
+
+  static final Color textColor = Color(0xFF5CC3FF);
   static final Color subtextColor = Color(0xFF7FBCF0);
+
   static final Color blackColor = Color(0xff19191b);
   static final Color greyColor = Color(0xff8f8f8f);
   static final Color userCircleBackground = Color(0xff2b2b33);
-  static final Color onlineDotColor = Color(0xFF30D158);
-  static final Color offlineDotColor = Color(0xFFFF3B30);
-  static final Color waitingDotColor = Color(0xFFE19500);
+
+  static final Color onlineDotColor = Color(0xFF5CFF72);
+  static final Color offlineDotColor = Color(0xFFFF2200);
+  static final Color waitingDotColor = Color(0xFFF4FF5C);
+
   static final Color separatorColor = Color(0xFFDEEEFB);
 
   static final Color gradientColorStart = Color(0xFF1990E4);
@@ -22,8 +36,5 @@ class UniversalVariables {
   static final Color senderColor = Color(0xFF6AB1E4);
   static final Color receiverColor = Color(0xFF41A0E3);
 
-  static final Gradient fabGradient = LinearGradient(
-      colors: [gradientColorStart, gradientColorEnd],
-      begin: Alignment.topLeft,
-      end: Alignment.bottomRight);
+  static final Color fabColor = Color(0xFF5CC3FF);
 }
